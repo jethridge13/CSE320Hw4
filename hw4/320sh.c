d6d8956432fe3127bc392440f076d69a10e1a5a1
jethridge <jethridge@cs.stonybrook.edu>
2016-03-30 14:16:21 -0400

    All built-in functions working.

--- hw4/320sh.c
+++ hw4/320sh.c
@@ -17,6 +17,10 @@ main (int argc, char ** argv, char **envp) {
 
   char* cmdDNE = ": command not found\n";
 
+  char previousWD[PWD_BUFFER_SIZE];
+  memset(previousWD, 0, PWD_BUFFER_SIZE);
+  getcwd(previousWD, PWD_BUFFER_SIZE);
+
 
   while (!finished) {
     char *cursor;
@@ -27,6 +31,13 @@ main (int argc, char ** argv, char **envp) {
     /* TODO Delete this */
     memset(cmds, 0, 100);
 
+    // Print the working directory
+    char cwd[PWD_BUFFER_SIZE];
+    memset(cwd, 0, PWD_BUFFER_SIZE);
+    getcwd(cwd, PWD_BUFFER_SIZE);
+    write(1, "[", 1);
+    write(1, cwd, PWD_BUFFER_SIZE);
+    write(1, "] ", 2);
 
     // Print the prompt
     rv = write(1, prompt, strlen(prompt));
@@ -76,12 +87,12 @@ main (int argc, char ** argv, char **envp) {
 
     // Execute the command, handling built-in commands separately 
     //Built-in commands
-    /* TODO Implement Built-in commands */
     char* exitCompare = "exit";
     char* cdCompare = "cd";
     char* pwdCompare = "pwd";
     char* echoCompare = "echo";
-    char* setCompare = "help";
+    char* setCompare = "set";
+    char* helpCompare = "help";
     if(cmdOne == NULL){
       /*  Input is just a newline.
           Do nothing. 
@@ -91,9 +102,26 @@ main (int argc, char ** argv, char **envp) {
       exit(0);
     } else if(!strcmp(cmdOne, cdCompare)){
       /* cd */
-      int returnCode = chdir(cmds[1]);
+      int returnCode = 0;
+
+      /* Temp will hold the current directory
+        which becomes the previous directory upon change */
+      char temp[PWD_BUFFER_SIZE];
+      memset(temp, 0, PWD_BUFFER_SIZE);
+      getcwd(temp, PWD_BUFFER_SIZE);
+
+      if(cmds[1] == NULL){
+        returnCode = chdir(getenv("HOME"));
+        strcpy(previousWD, temp);
+      } else if (!strcmp(cmds[1], "-")){
+        returnCode = chdir(previousWD);
+        strcpy(previousWD, temp);
+      } else {
+        returnCode = chdir(cmds[1]);
+        strcpy(previousWD, temp);
+      }
       if(returnCode){
-        write(1, "Something went wrong.", MAX_INPUT);
+        write(1, "Something went wrong.\n", 22);
       }
     } else if(!strcmp(cmdOne, pwdCompare)){
       /* pwd */
@@ -104,10 +132,47 @@ main (int argc, char ** argv, char **envp) {
       write(1, "\n", 1);
     } else if(!strcmp(cmdOne, echoCompare)){
       /* echo */
-
+      char* variable = getenv(cmds[1]);
+      if(variable != NULL){
+        int i = 0;
+        while(strcmp(&variable[i],"\0")){
+          i++;
+        }
+        write(1, variable, i);
+      } else {
+          int i = 1;
+          while(cmds[i] != NULL){
+            char* string = cmds[i];
+            int j = 0;
+            while(strcmp(&string[j], "")){
+              j++;
+            }
+            write(1, string, j);
+            write(1, " ", 1);
+            i++;
+          }
+        }
+      write(1, "\n", 1);
     } else if(!strcmp(cmdOne, setCompare)){
       /* set */
-
+      int returnCode = setenv(cmds[1], cmds[3], 1);
+      if(returnCode){
+        write(1, "Something went wrong.\n", 22);
+      }
+    } else if (!strcmp(cmdOne, helpCompare)){
+      /* help */
+      char* cdString = "cd\t\tChange the current working directory.\n";
+      char* echoString = "echo\t\tPrint strings and expand environment variables.\n";
+      char* exitString = "exit\t\tExits the shell.\n";
+      char* helpString = "help\t\tDisplay this help menu.\n";
+      char* pwdString = "pwd\t\tPrint the current working directory.\n";
+      char* setString = "set\t\tSet the value of environment variables.\n";
+      write(1, cdString, 42);
+      write(1, echoString, 54);
+      write(1, exitString, 23);
+      write(1, helpString, 30);
+      write(1, pwdString, 42);
+      write(1, setString, 45);
     } else {
       /* TODO Implement control flow to test if given command exists */
       /* Non-built in commands */
